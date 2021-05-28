109783791fd0b56b16990de0853367f1c63acd86
Djo917 <trstenjak.jonathan@gmail.com>
2021-05-28 14:30:49 +0200

    tous les label ont étaient liés aux input correspondant, la fonction de vérfication du nombre de tournoi a ete ajoute et est fonctionnelle. La fonction des checkbox a ete ajoute et est fonctionnelle.

--- js/modal.js
+++ js/modal.js
@@ -36,16 +36,13 @@ const closeForm = () => {
   modalBg.style.display = "none";
 }
 
-
-let messageErreurPrenom = document.getElementById("messageErreurPrenom");
-let messageErreurNom = document.getElementById("messageErreurNom");
-
-
 const firstNameValid = () => {
+  const messageErreurPrenom = document.getElementById("messageErreurPrenom");
   let prenom = document.getElementById("first");
   if(prenom.value.length < 2) {
     console.log("error");
-    messageErreurPrenom.textContent = "Le champ prénom doit contenir au minimum 2 caractères";
+    messageErreurPrenom.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
+    ;
     return false;
   }
   else {
@@ -55,10 +52,13 @@ const firstNameValid = () => {
 };
 
 const lastNameValid = () => {
+  const messageErreurNom = document.getElementById("messageErreurNom");
   let nom = document.getElementById("last");
+
   if(nom.value.length < 2) {
     console.log("error");
-    messageErreurNom.textContent = "Le champ nom doit contenir au minimum 2 caractères";
+    messageErreurNom.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    ;
     return false;
   }
   else {
@@ -67,10 +67,83 @@ const lastNameValid = () => {
   }
 };
 
+const emailAdressValid = () => {
+  const messageErreurMail = document.getElementById("messageErreurMail");
+  let email = document.getElementById("email");
+  const emailregex = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+
+  if(emailregex.test(email.value)) {
+    messageErreurMail.textContent = "";
+    return true;
+
+  }
+  else {
+    messageErreurMail.textContent = "L'adresse e-mail est incorrecte.";
+    return false;
+  }
+}
+
+const concoursNumber = () => {
+  const messageErreurConcours = document.getElementById("messageErreurConcours");
+  let quantity = document.getElementById("quantity");
+  const concoursregex = /[0-9]/;
+
+  if(concoursregex.test(quantity.value)) {
+    messageErreurMail.textContent = "";
+    return true;
+  }
+  else {
+    messageErreurConcours.textContent = "La valeur saisie n'est pas un nombre";
+    return false;
+  }
+}
+
+const checkboxChecked = () => {
+  const messageErreurCheckBox = document.getElementById("messageErreurCheckbox");
+
+  do {
+    let box = document.getElementById("location1");
+
+    if(box.checked == true) {
+      console.log("ok");
+      return true;
+    }
+    else if(document.getElementById("location2").checked == true) {
+      console.log("ok");
+      return true;  
+    }
+    else if(document.getElementById("location3").checked == true) {
+      console.log("ok");
+      return true;  
+    }
+    else if(document.getElementById("location4").checked == true) {
+      console.log("ok");
+      return true;  
+    }
+    else if(document.getElementById("location5").checked == true) {
+      console.log("ok");
+      return true;  
+    }
+    else if(document.getElementById("location6").checked == true) {
+      console.log("ok");
+      return true;  
+    }
+    else {
+      console.log("pas ok");  
+      messageErreurCheckBox.textContent = "Il faut cocher une case";
+      return false; 
+    }
+  }
+  while(box.checked != true)
+}
+
 document.getElementById("formulaire").addEventListener("submit", (test) => {
   test.preventDefault();
   firstNameValid();
   lastNameValid();
+  emailAdressValid();
+  checkboxChecked();
+  concoursNumber();
   console.log(test);
 });
 
