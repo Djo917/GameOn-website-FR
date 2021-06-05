3727d930543ed9f9ce57d66d2d15de884262f56f
Djo917 <trstenjak.jonathan@gmail.com>
2021-06-05 12:59:07 +0200

    Ajout de la règle des 16 ans. Ajout d'une règle pour les personnes agés de plus de 99 ans. Affichage du message d'erreur en cas de date non saisie

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,18 @@
 const init = () => {
   const modalBtn = document.querySelectorAll(".modal-btn");
-  const closed = document.getElementById('close');
+  const closed = document.getElementById("close");
   const icon = document.getElementById("icon");
 
   // launch modal event
-  modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-  closed.addEventListener('click', () => {
+  closed.addEventListener("click", () => {
     closeForm();
-  })
+  });
 
-  icon.addEventListener('click', () => {
+  icon.addEventListener("click", () => {
     editNav();
-  })
+  });
 };
 
 const editNav = () => {
@@ -30,22 +30,22 @@ const launchModal = () => {
   modalBg.style.display = "block";
 };
 
-
 const closeForm = () => {
   const modalBg = document.querySelector(".bground");
   modalBg.style.display = "none";
-}
+};
 
 const firstNameValid = () => {
   const messageErreurPrenom = document.getElementById("messageErreurPrenom");
   let prenom = document.getElementById("first");
-  if(prenom.value.length < 2) {
+
+  if (prenom.value.length < 2) {
     console.log("error");
-    messageErreurPrenom.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
-    ;
+    messageErreurPrenom.textContent =
+      "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     return false;
-  }
-  else {
+
+  } else {
     messageErreurPrenom.textContent = "";
     return true;
   }
@@ -55,13 +55,12 @@ const lastNameValid = () => {
   const messageErreurNom = document.getElementById("messageErreurNom");
   let nom = document.getElementById("last");
 
-  if(nom.value.length < 2) {
+  if (nom.value.length < 2) {
     console.log("error");
-    messageErreurNom.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
-    ;
+    messageErreurNom.textContent ="Veuillez entrer 2 caractères ou plus pour le champ du nom.";
     return false;
-  }
-  else {
+
+  } else {
     messageErreurNom.textContent = "";
     return true;
   }
@@ -72,92 +71,141 @@ const emailAdressValid = () => {
   let email = document.getElementById("email");
   const emailregex = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
 
-  if(emailregex.test(email.value)) {
+  if (emailregex.test(email.value)) {
     messageErreurMail.textContent = "";
     return true;
-  }
-  else {
+
+  } else {
     messageErreurMail.textContent = "L'adresse e-mail est incorrecte.";
     return false;
   }
-}
+};
 
 const concoursNumber = () => {
   const messageErreurConcours = document.getElementById("messageErreurConcours");
   const quantity = document.getElementById("quantity");
   const quantityNumber = parseInt(quantity.value, 10);
 
-  if(Number.isInteger(quantityNumber)) {
-    if(quantityNumber < 0 || quantityNumber > 99) {
-      messageErreurConcours.textContent = "Le nombre doit être compris en 0 et 99";
+  if (Number.isInteger(quantityNumber)) {
+    if (quantityNumber < 0 || quantityNumber > 99) {
+      messageErreurConcours.textContent ="Le nombre doit être compris en 0 et 99";
       return false;
     }
-      messageErreurConcours.textContent = "";
-      return true;
+    messageErreurConcours.textContent = "";
+    return true;
   }
-    messageErreurConcours.textContent = "La valeur saisie n'est pas un nombre";
-    return false;
-}
+  messageErreurConcours.textContent = "La valeur saisie n'est pas un nombre";
+  return false;
+};
 
 const checkboxChecked = () => {
   const messageErreurCheckBox = document.getElementById("messageErreurCheckbox");
 
   do {
-    let box = document.getElementById("location1");
+    const box = document.getElementById("location1");
 
-    if(box.checked == true) {
+    if (box.checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
       return true;
-    }
-    else if(document.getElementById("location2").checked == true) {
+
+    } else if (document.getElementById("location2").checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
-      return true;  
-    }
-    else if(document.getElementById("location3").checked == true) {
+      return true;
+
+    } else if (document.getElementById("location3").checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
-      return true;  
-    }
-    else if(document.getElementById("location4").checked == true) {
+      return true;
+
+    } else if (document.getElementById("location4").checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
-      return true;  
-    }
-    else if(document.getElementById("location5").checked == true) {
+      return true;
+
+    } else if (document.getElementById("location5").checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
-      return true;  
-    }
-    else if(document.getElementById("location6").checked == true) {
+      return true;
+
+    } else if (document.getElementById("location6").checked == true) {
       console.log("ok");
       messageErreurCheckBox.textContent = "";
-      return true;  
-    }
-    else {
-      console.log("pas ok");  
+      return true;
+
+    } else {
+      console.log("pas ok");
       messageErreurCheckBox.textContent = "Vous devez choisir une option";
-      return false; 
+      return false;
     }
-  }
-  while(box.checked != true)
-}
+  } while (box.checked != true);
+};
 
 const generalTerms = () => {
   const messageErreurGeneralTerms = document.getElementById("messageErreurGeneralTerms");
 
-  if(document.getElementById("checkbox1").checked == true) {
+  if (document.getElementById("checkbox1").checked == true) {
     console.log("ok");
     messageErreurGeneralTerms.textContent = "";
     return true;
-  }
-  else {
+
+  } else {
     console.log("pas ok");
-    messageErreurGeneralTerms.textContent = "Il faut accepter les conditions d utilisation";
+    messageErreurGeneralTerms.textContent ="Vous devez vérifier que vous acceptez les termes et conditions";
     return false;
   }
-}
+};
+
+const birthDate = () => {
+  const messageErreurBirthDate = document.getElementById("messageErreurBirthDate");
+  const date = document.getElementById("birthdate").value.split("-"); //enlève le séparateur de la date
+  const yearDays = date[0];
+  
+  const today =  new Date(); //Récupère la date actuelle
+  const currentYear = today.getFullYear(); // Stock l'année dans currentYear
+  
+  let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+
+  console.log(date, date[0] !== "");
+
+  if (date[0] !== "") {
+    /*Vérifie que le nombre de jour n'est pas supérieur à 30 pour les mois concernés*/
+    if (date[2] < 1) {
+      messageErreurBirthDate.textContent = "Le nombre du jour est incorrect";
+      return false;
+
+    } else if (date[1] < 1 || date[1] > 12) {
+      messageErreurBirthDate.textContent = "Le chiffre du mois est incorrect";
+      return false;
+
+    } else if (date[0].length < 4 || date[0].length > 4 || (currentYear - date[0]) > 99) {
+      messageErreurBirthDate.textContent = "L'année est incorrecte";
+      return false;
+    }
+
+    /* Définit si l'année est bissextile ou non
+  Dans le cas où elle l'est, le nombre de jour de février augmente de 1 */
+    if (yearDays % 400 === 0 || (yearDays % 100 !== 0 && yearDays % 4 === 0)) {
+      daysInMonth[1] = 29;
+    }
+    if (date[2] > daysInMonth[date[1]-1]) {
+      messageErreurBirthDate.textContent ="Le nombre de jour est incorrect pour ce mois";
+      return false;
+    }
+
+    if ((currentYear - date[0]) < 16) {
+      messageErreurBirthDate.textContent ="Il faut avoir 16 ans minimum pour s'inscrire";
+      return false;
+    }
+    messageErreurBirthDate.textContent ="";
+    return true;
+    
+  } else {
+    messageErreurBirthDate.textContent ="Vous devez entrer votre date de naissance.";
+  }
+};
 
 document.getElementById("formulaire").addEventListener("submit", (test) => {
   test.preventDefault();
@@ -167,8 +215,8 @@ document.getElementById("formulaire").addEventListener("submit", (test) => {
   checkboxChecked();
   concoursNumber();
   generalTerms();
+  birthDate();
   console.log(test);
 });
 
 init();
-
